cdb464dfb9280a8623ab1fa0fb8e8dd607337102
Afonso Martins <shark05228@sharkcoders.pt>
2024-02-01 13:00:14 +0000

    update 5

---
+++ pares_impares_while.py
@@ -0,0 +1,27 @@
+while True:
+
+    numero = int(input("Insira um número inteiro"))
+
+    if numero % 2 == 0 :
+        print("E o numero par")
+
+    else:
+        print ("E o numero impar")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
